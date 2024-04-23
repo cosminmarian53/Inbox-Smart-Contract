35a2a71fceebae46ecd0828a7eb90e307c70fa59
Lavric Cosmin-Marian <cosmin.lavric@student.unitbv.ro>
2024-04-23 22:51:56 +0300

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -4,7 +4,7 @@ const { abi, evm } = require("./compile");
 
 const provider = new HDWalletProvider(
   "betray march select unable zero trouble small render obey off click kangaroo",
-  "https://sepolia.infura.io/v3/bd333d08c1d44733b65868a1f2a6993b"
+  "https://sepolia.infura.io/v3/<YOUR_API_KEY>"
 );
 const web3 = new Web3(provider);
 
